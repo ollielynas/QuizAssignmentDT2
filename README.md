6f90fc705e8919759368b2869db663f19131938e
ollielynas <49426014+ollielynas@users.noreply.github.com>
2021-08-20 13:27:44 +1200

    added a new incorrect system

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # QuizAssignmentDT2
- my plan is to create a command line quiz that can use a JSON files to produce a the questions and answers. inside the json file there will be a dictionary of questions and answers. the program will read the file and will be able to use it to produce a quiz. the quiz will be about the names of famous people. the user will answer by writing the name of the person. yu will be give a description of the person. i also hope to at some point advance it from a command line app to a ui based app.
+ my plan is to create a quiz app that can use a JSON files to produce a the questions and answers. inside the json file there will be a dictionary of questions and answers. the program will read the file and will be able to use it to produce a quiz. the quiz will be about the names of famous people. the user will answer by writing the name of the person. yu will be give a description of the person. i also hope to at some point advance it from a command line app to a ui based app.
  <br>
  <br>
 <a href="https://docs.google.com/document/d/1YEYmNcoKA3OZtZaOfMmidjnEeX_0ip5HQU4zY-r9T6M/edit?usp=sharing">evidence document link</a>
@@ -19,3 +19,5 @@ these are things i want to add in later versions
 - [x] timer
 - [ ] tkinter UI
 
+## V1
+command line quiz app that asks questions and when you get something wrong it adds a point to a counter before asking you the question again later on
--- quizV2.py
+++ quizV2.py
@@ -6,6 +6,8 @@ import time
 #reading the json file that the questions are kept in
 Q_and_A = json.load(open("famousPerson.json", "r", encoding="utf-8"))
 
+os.system("CLS")
+
 # this allow people who allready know how to play a chance skip the tutorial while letting people who don't know how to play learn how to
 start_input = input(
     "hello and welcome to my quiz\npress enter to begin or type \"?\" to read a tutorial\n")
@@ -15,24 +17,37 @@ if start_input == "?":
 mistakes = 0
 tic = time.perf_counter()
 while True:
+    os.system("CLS")
+    display_list = []
     if len(list(Q_and_A.keys())) == 0:#this part checks if there are any more question left and if not it ends the loop which ends the quiz
         break
     answer = choice(list(Q_and_A.keys()))
     question = (Q_and_A[answer])
+    answer_list = list(answer)
+    for i in answer_list:
+        if i == " ":
+            display_list.append(" ")
+        else:
+            display_list.append("_")
 
-    os.system("CLS")
+    print("(",*display_list,")")
     user_input = input(question+"\n>")
 
     #this part mean that if the user adds an extra space before they answer or they capitalize their answer it will still be seen as correct
     processed_input = user_input.replace(" ", "").lower()
     processed_answer = answer.replace(" ", "").lower()
-
-    if processed_answer == processed_input:
-        print("correct")#this will run if they are right
-        del Q_and_A[answer]#it will also remove the question from the list of questions
-    else:
-        print("incorrect") # if they are incorrect, it will add a point to the counter and will ask them again later
-        mistakes += 1 
+    
+    while True:
+        if processed_answer == processed_input:
+            print("correct")#this will run if they are right
+            del Q_and_A[answer]#it will also remove the question from the list of questions
+            break
+        else:
+            os.system("CLS")
+            print("(",*display_list,")")
+            print(question)
+            user_input = input("incorrect\n>")
+            mistakes += 1 
     input("press enter for next question\n")
 toc = time.perf_counter()
 os.system("CLS")
